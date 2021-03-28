8ca4c507cd868735d4ff2a60677e0c1af5aa9fdf
datnt1298 <lfh275@gmail.com>
2021-03-28 10:01:15 +0700

    feat: add code: 1108. Defanging an IP Address

---
+++ 1106.go
@@ -0,0 +1,13 @@
+/* 1108. Defanging an IP Address
+https://leetcode.com/problems/defanging-an-ip-address/
+
+Given a valid (IPv4) IP address, return a defanged version of that IP address.
+
+A defanged IP address replaces every period "." with "[.]".
+*/
+
+func defangIPaddr(address string) string {
+
+	return strings.ReplaceAll(address, ".", "[.]")
+
+}
