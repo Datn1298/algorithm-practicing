91ed3969bc6a22be543f6ec047cc51de2adc119e
datnt1298 <lfh275@gmail.com>
2021-04-11 15:10:45 +0700

    feat: add code: 66. Plus One

---
+++ 66.go
@@ -0,0 +1,25 @@
+/* 66. Plus One
+https://leetcode.com/problems/plus-one/
+
+Given a non-empty array of decimal digits representing a non-negative integer, increment one to the integer.
+
+The digits are stored such that the most significant digit is at the head of the list, and each element in the array contains a single digit.
+
+You may assume the integer does not contain any leading zero, except the number 0 itself.
+*/
+
+func plusOne(digits []int) []int {
+	for i := len(digits) - 1; i >= 0; i-- {
+		if digits[i] < 9 {
+			digits[i] += 1
+			return digits
+			break
+		} else {
+			digits[i] = 0
+		}
+	}
+
+	res := make([]int, len(digits)+1)
+	res[0] = 1
+	return res
+}
